b5b300ce0332e45054d5cfa555f82aefd6268b81
Patryk Uran <trykpa@gmail.com>
2017-05-20 11:47:04 +0200

    fix scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@ var prefix = "https://cors-anywhere.herokuapp.com/";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
     $.getJSON(prefix + quoteUrl, createTweet);
     $.ajaxSetup({ 
     	cache: false
